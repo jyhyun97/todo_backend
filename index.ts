9736905b3b0841db74cf5875be0c34ec8ba5f4a0
jeonhyun <jeonhyun@student.42seoul.kr>
2023-10-22 23:24:02 +0900

    build : node + express + ts 세팅

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+    res.send('welcome!');
+});
+
+app.listen('3001', () => {
+    console.log(`
+  ################################################
+  🛡️  Server listening on port: 3001🛡️
+  ################################################
+`);
+});
+
